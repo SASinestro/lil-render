bef3456592a452b9d19a32da456723bbb745ca02
Alexis Williams <sasinestro@gmail.com>
2016-06-11 20:06:58 -0700

    WHAT IS THIS I AM DOG

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,16 @@
 module Main where
 
-import Image.TGA
+import Image
+import Image.Color
 
+red_color = RGBColor {
+      _red = 255
+    , _green = 0
+    , _blue = 0
+    , _alpha = 255
+}
 
 main :: IO ()
-main = return ()
+main = do
+    let image = make_image 50 50 red_color
+    write_image TGA "red.tga" image

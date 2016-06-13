57ce3eed8467e42bb189796b0534effac4e72168
Jon Williams <sasinestro@gmail.com>
2016-06-13 10:58:10 -0700

    Use `lens` better and have a useful Show instance for Image

--- src/Image.hs
+++ src/Image.hs
@@ -7,24 +7,39 @@ import Control.Monad
 import Control.Lens
 import Data.Vector ((!))
 import qualified Data.Vector as V
-import Data.Ix
 import Data.Foldable
+import Data.Ix
 
 type ImageIndexType = (Int, Int)
+
 
 data Image = Image {
       _storage :: V.Vector RGBColor
     , _width :: Int
     , _height :: Int
-} deriving (Show, Eq)
-
-makeLenses ''Image
+} deriving (Eq)
 
 (<!>) :: Image -> ImageIndexType -> RGBColor
-img <!> (x, y) = (img ^. storage) ! (w * y + x)
+img <!> (x, y) = (_storage img) ! (w * y + x)
                 where
-                    w = img ^. width
-                    h = img ^. height
+                    w = _width img
+                    h = _height img
+
+instance Show Image where
+    show img = foldl (++) header $ fmap line $ grid w h
+        where
+            grid :: Int -> Int -> [[ ImageIndexType ]]
+            grid x y = fmap range [ ( (i, 0), (i, y - 1) ) | i <- [0 .. x - 1]]
+
+            w = _width img
+            h = _height img
+
+            header = "Image (" ++ (show w) ++ "x" ++ (show h) ++ ")\n"
+
+            line :: [ ImageIndexType ] -> String
+            line (idx:idxs) = (foldl (\accum x -> accum ++ " " ++ (show $ img <!> x)) ("    " ++ (show $ img <!> idx)) idxs) ++ "\n"
+
+makeLenses ''Image
 
 class ImageConvertible a where
     toImage :: a -> Image
@@ -50,14 +65,14 @@ make_image width height color = Image {
 instance ImageConvertible TGAImage where
     toImage tga = Image storage height width
         where
-            height = fromIntegral . fromLE $ tga ^. tga_header ^. tga_height
-            width  = fromIntegral . fromLE $ tga ^. tga_header ^. tga_width
+            height = fromIntegral . fromLE $ tga ^. tga_header . tga_height
+            width  = fromIntegral . fromLE $ tga ^. tga_header . tga_width
             color_map = tga ^. tga_color_map
             storage = V.fromList $ case tga ^. tga_image_data of
                 (TGAIndexedData indexes) -> fmap (\index -> color_map ! fromIntegral index) indexes
                 (TGAUnmappedData colors) -> colors
     fromImage image = TGAImage {
-          _tga_header = simple_tga_header (image ^. width) (image ^. height)
+          _tga_header = simple_tga_header (_width image) (_height image)
         , _tga_color_map = V.empty
-        , _tga_image_data = TGAUnmappedData $ (V.toList $ image ^. storage)
+        , _tga_image_data = TGAUnmappedData $ (V.toList $ _storage image)
     }
